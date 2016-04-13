4c13ecd6e744e2a7942c4cecd4924aebb93db0d7
Skylar Anderson <skylar@aptible.com>
2016-04-13 15:09:25 -0700

    allow array property values to be used to satisfy dependency checks

--- tests/integration/dependent-properties-test.js
+++ tests/integration/dependent-properties-test.js
@@ -2,6 +2,39 @@ import { moduleForComponent, test } from 'ember-qunit';
 import Schema from 'ember-json-schema-document/models/schema';
 import hbs from 'htmlbars-inline-precompile';
 
+let arrayDependencySchema = {
+  '$schema': 'http://json-schema.org/draft-04/schema#',
+  'id': 'http://jsonschema.net',
+  'type': 'object',
+  'properties': {
+    'vulnerabilityScanner': {
+      'type': 'object',
+      'properties': {
+        'provider': {
+          'type': 'array',
+          'title': 'Which provider do you use?',
+          'items': {
+            'type': 'string',
+            'enum': [
+              'Amazon',
+              'Google',
+              'Other'
+            ]
+          }
+        },
+
+        'other': {
+          'title': 'Which other provider do you use?',
+          'type': 'text'
+        }
+      },
+
+      'required': [ 'provider' ],
+      '_dependencies': { 'other': { 'provider': 'Other' } }
+    }
+  }
+};
+
 let schemaBody = {
   '$schema': 'http://json-schema.org/draft-04/schema#',
   'id': 'http://jsonschema.net',
@@ -79,4 +112,26 @@ test('renders dependent properties', function(assert) {
   assert.equal(this.$('select[name="vulnerabilityScanner.frequency"]').length, 1, 'shows frequency select');
   assert.equal(this.$('input[name="vulnerabilityScanner.whyNot"]').length, 0, 'doesn\'t shows whynot text');
 
+});
+
+test('renders dependent properties with array values', function(assert) {
+  let schema = new Schema(arrayDependencySchema);
+  let { properties } = schema;
+  let document = schema.buildDocument();
+
+  this.setProperties({ document, properties });
+
+  this.render(hbs`
+    {{#each-property properties=properties document=document as |key property type options|}}
+      {{#if options.showProperty}}
+        {{component (concat 'schema-field-' type) key=key property=property document=document}}
+      {{/if}}
+    {{/each-property}}
+  `);
+
+  assert.equal(this.$('.checkbox label span:contains(Amazon)').length, 1, 'shows provider options');
+  assert.equal(this.$('input[name="vulnerabilityScanner.other"]').length, 0, 'doesn\'t show other');
+
+  this.$('.checkbox label span:contains(Other)').click();
+  assert.equal(this.$('input[name="vulnerabilityScanner.other"]').length, 1, 'other is visible');
 });
